b6fba82b9a90533868d8b9f5750fd03d41dfc0aa
Manh Le <manhlq91@gmail.com>
2019-09-16 23:12:52 +0700

    Update id type

--- ngheluon/src/main/java/saola/com/ngheluon/dataset/BaseModel.java
+++ ngheluon/src/main/java/saola/com/ngheluon/dataset/BaseModel.java
@@ -3,14 +3,10 @@ package saola.com.ngheluon.dataset;
 import java.io.Serializable;
 import java.util.UUID;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-
-import org.hibernate.annotations.GenericGenerator;
 
 import lombok.Data;
 
@@ -19,12 +15,13 @@ import lombok.Data;
 public class BaseModel implements Serializable {
   private static final long serialVersionUID = 1L;
   @Id
-  @GeneratedValue(generator = "UUID")
-  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-  @Column(name = "id", updatable = false, nullable = false)
-  private UUID id;
+  private String id;
   private Long created;
   private Long updated;
+
+  public BaseModel() {
+    this.id = UUID.randomUUID().toString();
+  }
 
   @PrePersist
   protected void prePersist() {
